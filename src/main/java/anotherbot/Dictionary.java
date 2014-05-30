d3c91756f81d8b5f172463f3eebd362013ba9726
shrdy <sherardy@users.noreply.github.com>
2014-05-29 21:51:43 -0400

    Dictionary.java dictionary writing test
    again, horrendous code, most of it is probably useless

--- src/main/java/anotherbot/Dictionary.java
+++ src/main/java/anotherbot/Dictionary.java
@@ -1,6 +1,7 @@
 package anotherbot;
 
 import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -21,10 +22,13 @@ public class Dictionary {
         }
     }
 
-    public void save(Collection<String> lines) { // a collection can be a set, a
-                                                 // list, etc
-        ArrayList<String> contents = new ArrayList<String>(lines);
-        Iterator<String> it = contents.iterator();
+    public void save(Collection<String> lines) throws FileNotFoundException,
+            IOException { // a collection can be a set, a
+        // list, etc
+        ArrayList<String> contents = new ArrayList<String>(
+                Util.getFileContents(this.filename));
+        ArrayList<String> linesToWrite = new ArrayList<String>(lines);
+        Iterator<String> it = linesToWrite.iterator();
         try (PrintWriter out = new PrintWriter(new BufferedWriter(
                 new FileWriter(filename, true)))) {
             while (it.hasNext()) {
@@ -41,4 +45,37 @@ public class Dictionary {
     public String getFilename() {
         return this.filename;
     }
+
+    public void saveTest(String word, String nextWord)
+            throws FileNotFoundException, IOException {
+        String[] leftSide =Util.getFileContents(this.filename).toString().split("\n");
+        int i=0;
+        String[] split;
+        for (String element : leftSide) {
+            split=element.split(":");
+            leftSide[i]=split[0];
+            i++;
+        }
+        System.out.println(leftSide[0]);
+        int findWord;
+        // ArrayList<String> linesToWrite = new ArrayList<String>(lines);
+     //   Iterator<String> it = contents.iterator();
+        findWord = leftSide[0].lastIndexOf(word);
+        if (findWord == -1) {
+            try (PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new FileWriter(filename, true)))) {
+                out.println(word+":"+nextWord);
+                //out.print(": ");
+       //         while (!it.next().equals(word)) {
+        //            out.println(it.next()); // this will append anything to the
+                                            // end
+                    // of the file, in other words it wont
+                    // overwrite the file
+       //         }
+            } catch (IOException e) {
+                System.err.println(e);
+            }
+        }
+    }
+
 }
