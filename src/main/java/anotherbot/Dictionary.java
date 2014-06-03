49fb0f44ba176688ee90b761f3ea1960918c6a99
shrdy <sherardy@users.noreply.github.com>
2014-06-03 08:40:45 -0400

    stuff

--- src/main/java/anotherbot/Dictionary.java
+++ src/main/java/anotherbot/Dictionary.java
@@ -9,6 +9,8 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class Dictionary {
     // TODO
@@ -60,8 +62,36 @@ public class Dictionary {
             System.err.println(e);
         }
     }
+    
+    public Set<String> loadLeftSide() throws FileNotFoundException, IOException {
+    	 ArrayList<String> dictionaryContents = Util.getFileContents(this.filename);
+    	 Set<String> leftSideElements=new LinkedHashSet<String>();
+    	 String[] splitElement;
+    	 for (String element : dictionaryContents) {
+    		 splitElement=element.trim().split(SEPARATOR);
+    		 leftSideElements.add(splitElement[0]);
+    		 System.out.println("Left side loaded: "+leftSideElements.toString());
+    	 }
+		return leftSideElements;
+    }
+    
+    public ArrayList<String> loadRightSide() throws FileNotFoundException, IOException {
+    	ArrayList<String> dictionaryContents = Util.getFileContents(this.filename);
+    	ArrayList<String> rightSideElements=new ArrayList<String>();
+    	String[] splitElement;
+    	for (String element : dictionaryContents) {
+    		splitElement=element.trim().split(SEPARATOR);
+    		rightSideElements.add(splitElement[1]);
+    		System.out.println("Right side loaded: "+rightSideElements.toString());
+    	}
+		return rightSideElements;
+    }
 
-    public void saveLeftSide(String word) {
+    public void saveLeftSide(String word) throws FileNotFoundException, IOException {
+    	ArrayList<String> dictionaryContents = Util.getFileContents(this.filename);
+    	if (dictionaryContents.contains(word+SEPARATOR)) {
+    		return;
+    	}
         try (PrintWriter out = new PrintWriter(new BufferedWriter(
                 new FileWriter(filename, true)))) {
             out.println(word + ":");
@@ -72,8 +102,16 @@ public class Dictionary {
 
     public void saveRightSide(String leftSide, String wordRight)
             throws FileNotFoundException, IOException {
-        ArrayList<String> dictionaryContents = Util
-                .getFileContents(this.filename); // apparently to avoid the
+        ArrayList<String> dictionaryContents = Util.getFileContents(this.filename);
+        ArrayList<String> elements=new ArrayList<String>(dictionaryContents);
+        for (String element : dictionaryContents) {
+        	if (element.trim().contains(leftSide+SEPARATOR)) {
+        		elements.add(element+" "+wordRight);
+        		elements.remove(dictionaryContents.lastIndexOf(element+SEPARATOR)+1);
+        	}
+        }
+        save(elements);
+        // apparently to avoid the
         // nightmarish hell that is
         // RandomAccessFile we have to
         // look for the left side word
